136e8cc2455290666db6634b4e72fce3143940cd
Sperrotta10 <santiagoperrotta10@gmail.com>
2026-03-19 17:24:20 -0400

    feat: agregar scripts de Prisma y ajustar configuración de conexión a la base de datos

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,18 @@
+import { PrismaClient } from '../generated/prisma/index.js';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import { env } from "./env.js"
+
 
 // 1. Verificamos que la variable de entorno exista para evitar errores en producción
-const connectionString = process.env.DATABASE_URL;
+const connectionString = env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("Falta la variable DATABASE_URL en el archivo .env");
 }
 
 // 2. Creamos el Pool nativo
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString, ssl: { rejectUnauthorized: false } });
 
 // 3. Pasamos el pool al adaptador (Usamos 'as any' para calmar a TypeScript)
 const adapter = new PrismaPg(pool as any);
